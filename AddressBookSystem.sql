b51761057d5e905f2673531218b509252633e2ff
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-30 18:18:27 +0530

    [Afrath]Updated the file create table address table usecase

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -1,3 +1,15 @@
 -----UC-1 Create Address Book Service Database-----
 create database addressbook_service
 use addressbook_service
+-----UC-2 Ability to create address book table-----
+create table address_book_table
+(
+FirstName varchar(255),
+LastName varchar(255),
+Address varchar(255),
+City varchar(255),
+State varchar(255),
+ZipCode varchar(255),
+PhoneNumber bigint,
+email varchar(25)
+) 
