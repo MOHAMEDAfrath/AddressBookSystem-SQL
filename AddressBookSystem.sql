5f00560589ab0401ef8308e1dbf1c8f0e2e9f2a0
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-02 13:07:21 +0530

    [Afrath]Updated the file with insert same contact for two different types and created table based on ER

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -39,3 +39,44 @@ update address_book_table set AddressBookName = 'School',TypeOfAddressBook = 'Fr
 update address_book_table set AddressBookName = 'Manager',TypeOfAddressBook = 'Profession' where FirstName = 'Dhanush'
 -----UC-10 Count by Type-----
 select count(*) as noOfContacts,TypeOfAddressBook from address_book_table group by TypeOfAddressBook
+-----UC-11 Insert same contact as family and friend-----
+insert into address_book_table values('Amir','Khan','Ambattur','Chennai','TamilNadu','600660','7744556699','amir123@gmail.com','Home','Family')
+----Create tables based on ER----
+---create table contact list---
+create table contact_list
+(
+ContactId int identity(1,1) primary key,
+FirstName varchar(255),
+LastName varchar(255),
+Address varchar(255),
+City varchar(255),
+State varchar(255),
+ZipCode varchar(255),
+PhoneNumber bigint,
+email varchar(25),
+AddressBookName varchar(255)
+)
+insert into contact_list values('Ram','Kumar','Egmore','Chennai','TamilNadu','600078','9841578941','ram@gmail.com','Home'),
+('Uma','Devi','Menod','BPmark','UP','ZIP8531','7841526987','umas@gmail.com','Home'),
+('Amir','Khan','Ambattur','Chennai','Tamil Nadu','600660','7744556699','amir@gmail.com','School'),
+('Dhanush','Raj','XXXX','yyyy','Banglore','600XXX','4477885599','raj@gmail.com','Manager')
+select * from contact_list
+---create table address_book_type---
+create table address_book_type
+(
+	TypeId int identity(1,1) primary key,
+	AddressBookType varchar(255)
+)
+insert into address_book_type values('Friends'),('Family'),('Profession')
+select * from address_book_type
+---create table contact map---
+create table contact_map_type
+(
+contactId int,
+typeId int,
+foreign key(contactId) references contact_list(ContactId),
+foreign key(typeId) references address_book_type(TypeId)
+)
+insert into contact_map_type values('1','1'),('2','3'),('3','1'),('1','2'),('4','3')
+insert into contact_map_type values('2','1')
+select * from contact_map_type
